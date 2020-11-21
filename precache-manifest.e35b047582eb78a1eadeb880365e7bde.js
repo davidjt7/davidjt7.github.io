bb61da116894359806fc72ebfa52ef226d32b853
David Thomas <djt.in7@gmail.com>
2020-11-21 13:11:42 +0530

    Updates

---
+++ precache-manifest.e35b047582eb78a1eadeb880365e7bde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984f599efde25bd42c448cefd539d300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8723c7709805def616df",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8723c7709805def616df",
+    "url": "/static/js/main.caf92773.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "63505362484f86ecfd7ff2ca14ffc3cf",
+    "url": "/static/media/djt.63505362.jpeg"
+  },
+  {
+    "revision": "7261b5c884e2774d2773f13d0fcef4d9",
+    "url": "/static/media/resume.7261b5c8.pdf"
+  }
+]);
