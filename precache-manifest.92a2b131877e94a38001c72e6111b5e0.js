3e335152c6c8c2db6a4f59b91b0da7c083709b5d
David Thomas <djt.in7@gmail.com>
2020-12-08 09:07:29 +0530

    Updates

---
+++ precache-manifest.92a2b131877e94a38001c72e6111b5e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e7c21cc341a13aee60990dd9e91fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c057b57dbe072a89cced",
+    "url": "/static/css/main.e0abacc9.chunk.css"
+  },
+  {
+    "revision": "cf6401a6b68abeed97fa",
+    "url": "/static/js/2.d7c4ee0a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d7c4ee0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c057b57dbe072a89cced",
+    "url": "/static/js/main.dfc6a300.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3b92687d9c20a3b490e5aa05b11beec9",
+    "url": "/static/media/dash.3b92687d.jpeg"
+  },
+  {
+    "revision": "f50872dfb91a7fb7212a9175075ee1c6",
+    "url": "/static/media/djt.f50872df.jpeg"
+  },
+  {
+    "revision": "03fca38ec2f6fb21d9874d3434f17d9d",
+    "url": "/static/media/djts.03fca38e.jpeg"
+  },
+  {
+    "revision": "7261b5c884e2774d2773f13d0fcef4d9",
+    "url": "/static/media/resume.7261b5c8.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59e974489552f6d402f7e8f5d901a38a.js"
+  "/precache-manifest.92a2b131877e94a38001c72e6111b5e0.js"
 );
 
 self.addEventListener('message', (event) => {
